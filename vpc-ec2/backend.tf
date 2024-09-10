b0619433cc9decd41fb581573aeb30dc53506661
Abdihakim Said <abdihakimsaid1@gmail.com>
2024-09-11 02:18:55 +0530

    updated the backend

--- vpc-ec2/backend.tf
+++ vpc-ec2/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.9.5"
+  required_version = "~> 1.5.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,10 +11,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "tcp-devops-1x19x"
     region         = "us-east-1"
     key            = "vpc/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "tcp-devops-state-lock-table"
     encrypt        = true
   }
 }
